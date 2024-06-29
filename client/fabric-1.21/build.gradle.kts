7aa3ad2f1884f7eb119f42014d792288bf103156
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2024-06-29 18:49:45 +0200

    Created client/fabric-1.21 project

---
+++ client/fabric-1.21/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("java")
+    id("fabric-loom") version "1.6-SNAPSHOT"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val loaderVersion = "0.15.11"
+
+dependencies {
+    implementation(project(":common"))
+    minecraft("com.mojang:minecraft:1.21")
+    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.3+1.21")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    withSourcesJar()
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release = 21
+    }
+
+    processResources {
+        filesMatching("fabric.mod.json") {
+            expand(mapOf("version" to version))
+            expand(mapOf("loader_version" to loaderVersion))
+        }
+    }
+
+    jar {
+        from("LICENSE") {
+            rename { "${it}_$archiveBaseName" }
+        }
+    }
+}
---
+++ client/fabric-1.21/src/main/java/de/fabiexe/steroids/SteroidsFabricClient.java
@@ -0,0 +1,8 @@
+package de.fabiexe.steroids;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SteroidsFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
