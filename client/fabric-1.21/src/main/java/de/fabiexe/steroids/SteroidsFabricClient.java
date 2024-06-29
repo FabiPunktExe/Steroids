62de7dbe8463c1794ed038e9af8ec8d132b9659a
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2024-06-29 19:22:04 +0200

    Added toggle_builder_mode Key Binding

--- client/fabric-1.21/src/main/java/de/fabiexe/steroids/SteroidsFabricClient.java
+++ client/fabric-1.21/src/main/java/de/fabiexe/steroids/SteroidsFabricClient.java
@@ -1,8 +1,29 @@
 package de.fabiexe.steroids;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
 
 public class SteroidsFabricClient implements ClientModInitializer {
+    public static boolean BUILDER_MODE = false;
+    private final KeyMapping keyBinding =
+            new KeyMapping(
+                    "key.steroids.toggle_builder_mode",
+                    GLFW.GLFW_KEY_J,
+                    "categories.steroids.steroids");
+
     @Override
-    public void onInitializeClient() {}
+    public void onInitializeClient() {
+        KeyBindingHelper.registerKeyBinding(keyBinding);
+        ClientTickEvents.START_CLIENT_TICK.register(this::tick);
+    }
+
+    private void tick(Minecraft client) {
+        if (keyBinding.consumeClick()) {
+            BUILDER_MODE = !BUILDER_MODE;
+        }
+    }
 }
--- common/src/main/java/de/fabiexe/steroids/editable/Editable.java
+++ common/src/main/java/de/fabiexe/steroids/editable/Editable.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 public interface Editable {
     /**
      * Get the position of the object.
+     *
      * @return The position of the object.
      */
     @NotNull
@@ -15,6 +16,7 @@ public interface Editable {
 
     /**
      * Get the rotation of the object.
+     *
      * @return The rotation of the object.
      */
     @NotNull
@@ -22,42 +24,49 @@ public interface Editable {
 
     /**
      * Check if the object is rotatable around the x-axis.
+     *
      * @return true if the object is rotatable around the x-axis, otherwise false.
      */
     boolean isXAxisRotatable();
 
     /**
      * Set the rotation of the object around the x-axis.
+     *
      * @param rotation The rotation of the object around the x-axis.
      */
     void setXRotation(double rotation);
 
     /**
      * Check if the object is rotatable around the y-axis.
+     *
      * @return true if the object is rotatable around the y-axis, otherwise false.
      */
     boolean isYAxisRotatable();
 
     /**
      * Set the rotation of the object around the y-axis.
+     *
      * @param rotation The rotation of the object around the y-axis.
      */
     void setYRotation(double rotation);
 
     /**
      * Check if the object is rotatable around the z-axis.
+     *
      * @return true if the object is rotatable around the z-axis, otherwise false.
      */
     boolean isZAxisRotatable();
 
     /**
      * Set the rotation of the object around the z-axis.
+     *
      * @param rotation The rotation of the object around the z-axis.
      */
     void setZRotation(double rotation);
 
     /**
      * Get the size of the object.
+     *
      * @return The size of the object.
      */
     @NotNull
@@ -65,36 +74,42 @@ public interface Editable {
 
     /**
      * Check if the object is resizable along the x-axis.
+     *
      * @return true if the object is resizable along the x-axis, otherwise false.
      */
     boolean isXAxisResizable();
 
     /**
      * Resize the object along the x-axis.
+     *
      * @param size The new size of the object along the x-axis.
      */
     void resizeXAxis(double size);
 
     /**
      * Check if the object is resizable along the y-axis.
+     *
      * @return true if the object is resizable along the y-axis, otherwise false.
      */
     boolean isYAxisResizable();
 
     /**
      * Resize the object along the y-axis.
+     *
      * @param size The new size of the object along the y-axis.
      */
     void resizeYAxis(double size);
 
     /**
      * Check if the object is resizable along the z-axis.
+     *
      * @return true if the object is resizable along the z-axis, otherwise false.
      */
     boolean isZAxisResizable();
 
     /**
      * Resize the object along the z-axis.
+     *
      * @param size The new size of the object along the z-axis.
      */
     void resizeZAxis(double size);
